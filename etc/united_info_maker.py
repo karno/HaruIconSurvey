a7c85a2dc345815432f7cb45ebdec127ceb866ab
hnakai0909 <hnakai0909@yahoo.co.jp>
2015-09-20 02:07:03 +0900

    add transfer station list process (TODO:switch to use pprint)

--- etc/united_info_maker.py
+++ etc/united_info_maker.py
@@ -1,12 +1,12 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # united_info.jsonをある程度まで自動生成
 # (直通などは非対応なので手での修正が必要)
 # 引数:なし
 
 
-import sys
-import os
+import sys, os, pprint
+from haru_station_locations import station_locations
 from haru_stations import stations
 from haru_lines import lines, company_author, airline, ship
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -21,10 +21,6 @@ tab5 = '\t\t\t\t\t'.expandtabs(tabsize);
 def wrap(s):
 	return "\"" + s + "\"";
 
-def writeln(s):
-    write(s+'\n');
-    return;
-
 fr = open("haru_station_locations.py", "r", encoding="utf-8");
 fw = open("united_info_draft.json", "w", encoding="utf-8");
 
@@ -35,8 +31,10 @@ flag_initiated = False;
 fw.write('{\n');
 fw.write('    \"lines\": {\n');
 prev_exist_dist_info, exist_dist_info = False, False;
+prev_sta_options, sta_options = '', '';
 
-for line in fr:
+# parse haru_station_location.py manually
+for line in fr: # line = 行
     if line.expandtabs(tabsize).startswith(tab2+'#') :
         fw.write(tab2+line);
     # seek station data line
@@ -54,11 +52,15 @@ for line in fr:
         else :
             # write previous last line
             if prev_exist_dist_info :
-                fw.write(tab4 + '\"' + prev_sta_code + '\": { \"connect\": { \"' + \
-                prev_line_code + prev2_sta_code + '\": ' + prev_distance + ' } }\n');
-            else :
-                # when no distance info at the line(路線)
-                fw.write(tab4 + "# fixme\n");
+                if prev_sta_options == '' :
+                    fw.write(tab4 + '\"' + prev_sta_code + '\": { \"connect\": { \"');
+                    fw.write(prev_line_code + prev2_sta_code + '\": ' + prev_distance + ' } }\n');
+                else :
+                    fw.write(tab4 + '\"' + prev_sta_code + '\": {\n');
+                    fw.write(prev_sta_options);
+                    fw.write(tab5 + '\"connect\": { \"' + prev_line_code + \
+                    prev2_sta_code + '\": ' + prev_distance + ' }\n');
+                    fw.write(tab4 + '},\n');  
             fw.write(tab3 + '}\n');
             fw.write(tab2 + '},\n');
         fw.write(tab2 + '\"' + line_code + '\": {\n');
@@ -66,6 +68,10 @@ for line in fr:
         fw.write(tab3 + '\"company\": \"' + line_company + '\",\n');
         fw.write(tab3 + '\"author\": \"' + company_author.get(line_company,'(unknown)') + '\",\n');
         fw.write(tab3 + '\"color\": \"' + line_color + '\",\n');
+        if line_code in airline :
+            fw.write(tab3 + '\"airline\": true\n');
+        if line_code in ship :
+            fw.write(tab3 + '\"ship\": true\n');    
         fw.write(tab3 + '\"stations\": {\n');
         prev_exist_dist_info, exist_dist_info = False, False;
         continue;
@@ -79,32 +85,57 @@ for line in fr:
     sta_code = sta_code.strip('\"');
     comment = comment.strip();
     sta_name = sta_name.strip('\"');
-    station_options = '';
+    # station options
+    sta_options = '';
     if comment.startswith('<夜間通過>') :
-        station_options += (tab4 + '\"pass_night\": true\n');
+        sta_options += (tab5 + '\"pass_night\": true,\n');
     exist_dist_info = (comment.endswith('blocks') and comment.strip('blocks ').isdecimal());
     if exist_dist_info :
         distance = comment.strip('blocks ');
         # write *previous* station
         if prev_exist_dist_info :
             # when both prev and next distance info exist (distance info continue)
-            fw.write(tab4 + '\"' + prev_sta_code + '\": { \"connect\": { \"' + \
-            line_code + prev2_sta_code + '\": ' + prev_distance + ', \"' + \
-            line_code + sta_code + '\": ' + distance + ' } },\n');
+            if prev_sta_options == '' :
+                fw.write(tab4 + '\"' + prev_sta_code + '\": { \"connect\": { \"');
+                fw.write(line_code + prev2_sta_code + '\": ' + prev_distance + \
+                ', \"' + line_code + sta_code + '\": ' + distance + ' } },\n');
+            else :
+                fw.write(tab4 + '\"' + prev_sta_code + '\": {\n');
+                fw.write(prev_sta_options);
+                fw.write(tab5 + '\"connect\": { \"');
+                fw.write(line_code + prev2_sta_code + '\": ' + prev_distance + \
+                ', \"' + line_code + sta_code + '\": ' + distance + ' }\n');
+                fw.write(tab4 + '},\n');
         else :
             # when distance info begin
-            fw.write(tab4 + '\"' + prev_sta_code + '\": { \"connect\": { \"' + \
-            line_code + sta_code + '\": ' + distance + ' } },\n');
+            if prev_sta_options == '' :
+                fw.write(tab4 + '\"' + prev_sta_code + '\": { \"connect\": { \"');
+                fw.write(line_code + sta_code + '\": ' + distance + ' } },\n');
+            else :
+                fw.write(tab4 + '\"' + prev_sta_code + '\": {\n');
+                fw.write(prev_sta_options);
+                fw.write(tab5 + '\"connect\": { \"');
+                fw.write(line_code + sta_code + '\": ' + distance + ' }\n');
+                fw.write(tab4 + '},\n');
         prev2_sta_code = prev_sta_code;
         prev_sta_code, prev_distance = sta_code, distance;
-        prev_exist_dist_info = exist_dist_info;
+        prev_sta_options, prev_exist_dist_info = sta_options, exist_dist_info;
     else : 
         if prev_exist_dist_info : 
-            fw.write(tab4 + '\"' + prev_sta_code + '\": { \"connect\": { \"' + \
-            line_code + prev2_sta_code + '\": ' + prev_distance + ' } }\n');
+            if prev_sta_options == '' :
+                fw.write(tab4 + '\"' + prev_sta_code + '\": { \"connect\": { \"');
+                fw.write(line_code + prev2_sta_code + '\": ' + prev_distance + ' } }\n');
+            else :
+                fw.write(tab4 + '\"' + prev_sta_code + '\": {\n');
+                fw.write(prev_sta_options);
+                fw.write(tab5 + '\"connect\": { \"');
+                fw.write(line_code + prev2_sta_code + '\": ' + prev_distance + ' }\n');
+                fw.write(tab4 + '},\n');
+            
         prev_sta_code = sta_code;
-        prev_exist_dist_info = exist_dist_info;
+        prev_sta_options, prev_exist_dist_info = sta_options, exist_dist_info;
         continue;
+# for end (for line in fr)
 
 # finalize(write last last line)
 fw.write(tab3 + '}\n');
@@ -116,5 +147,23 @@ print("# phase 1 complete.");
 ######################################################
 
 print("# phase 2: transfer");
-
+transfer_sta_list = {}
 fw.write('    \"transfer\": {\n');
+for line_code in station_locations.keys() :
+    for sta_code in station_locations[line_code].keys() :
+        if not sta_code in transfer_sta_list :
+            transfer_sta_list[sta_code] = [];
+        transfer_sta_list[sta_code].append(line_code + sta_code);
+# delete no transfer station
+transfer_sta_list = { k : v for k,v in transfer_sta_list.items() if len(v)>=2}
+
+
+print("# phase 2 complete.");
+
+######################################################
+
+print("# phase 3: staions");
+print("# phase 3 complete.");
+
+# memo
+# fw.write(pprint.pformat(HOGE,indent=4,).maketrans('\'','\"')); 
